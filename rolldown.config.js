0c908c48109cd2d909bafd3406bbb59fad1c2a2e
Seb Heron <seb.heron@horiba.com>
2026-03-22 16:10:56 +0000

    Minify/remove comments to shrink package for npm

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from "rolldown";
 
+const googleLegal = `/**
+* @license
+* Copyright Google LLC All Rights Reserved.
+*
+* Use of this source code is governed by an MIT-style license that can be
+* found in the LICENSE file at https://angular.dev/license
+*/`
+
 export default defineConfig({
   input: "index.js",
   plugins: [
@@ -28,6 +36,10 @@ export default defineConfig({
     format: "esm",
     file: "build.js",
     intro: "var require;var document;var process;",
+    // Can't get this to work, leaving as a note.
     polyfillRequire: false,
+    minify: true,
+    comments: false,
+    postBanner: googleLegal,
   },
 });
